b75c91b5886aee4cc65093ab026e4a7ece9f1ce5
VishalWorksX <vishalkori1209@gmail.com>
2026-03-22 22:22:06 +0530

    Add files via upload

---
+++ Car Listing/All Cars/allcars.php
@@ -0,0 +1,619 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>All Cars</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="allcars.css">
+</head>
+<body>
+    <!--Maruti cars-->
+    <div class="car-container">
+    <div class="car-image">
+      <img src="alto.jpg" alt="Maruti alto">
+    </div>
+    <div class="car-info">
+      <a href="/AutoHub/index/maruti suzuki/Alto/Alto.php"><h2>Maruti Alto</h2></a>
+      <p>4.1/5 (390 Ratings)</p>
+      <p>22-30 kmpl | 56-66 bhp</p>
+      <p>Starting at Rs. 3.99 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="brezza.jpg" alt="Maruti Grand Vitara">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/maruti suzuki/Brezza/Brezza.php"><h2>Maruti Brezza</h2></a>
+      <p>4.4/5 (353 Ratings)</p>
+      <p>19-27 kmpl | 87-102 bhp</p>
+      <p>Starting at Rs. 10.76 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="fronx.jpg" alt="Maruti Fronx">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/maruti suzuki/Fronx/Fronx.php"><h2>Maruti Fronx</h2></a>
+      <p>4.5/5 (390 Ratings)</p>
+      <p>20-28 kmpl | 76-99 bhp</p>
+      <p>Starting at Rs. 7.51 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="swift.jpg" alt="Maruti Grand Vitara">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/maruti suzuki/Swift/Swift.php"><h2>Maruti Swift</h2></a>
+      <p>4.4/5 (353 Ratings)</p>
+      <p>22-30 kmpl | 76-89 bhp</p>
+      <p>Starting at Rs. 5.99 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="ertiga.jpg" alt="Maruti Fronx">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/maruti suzuki/Ertiga/Ertiga.php"><h2>Maruti Ertiga</h2></a>
+      <p>4.5/5 (390 Ratings)</p>
+      <p>20-26 kmpl | 87-102 bhp</p>
+      <p>Starting at Rs. 8.69 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="wagonr.jpg" alt="Maruti Grand Vitara">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/maruti suzuki/Wagonr/Wagonr.php"><h2>Maruti WagonR</h2></a>
+      <p>4.6/5 (353 Ratings)</p>
+      <p>23-34 kmpl | 56-89 bhp</p>
+      <p>Starting at Rs. 5.54 Lakh</p>
+    </div>
+  </div>
+<!--tata cars-->
+<div class="car-container">
+    <div class="car-image">
+      <img src="altroz.jpg" alt="Tata Altroz">
+    </div>
+    <div class="car-info">
+      <a href="/AutoHub/index/tata/Altroz/Altroz.php"><h2>Tata Altroz</h2></a>
+      <p>4.6/5 (390 Ratings)</p>
+      <p>18-26 kmpl | 72-108 bhp</p>
+      <p>Starting at Rs. 6.65 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="harrier.jpg" alt="Tata Harrier">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/tata/Harrier/Harrier.php"><h2>Tata Harrier</h2></a>
+      <p>4.4/5 (353 Ratings)</p>
+      <p>14-16 kmpl | 168 bhp</p>
+      <p>Starting at Rs. 15.49 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="nexon.jpg" alt="Tata Nexon">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/tata/Nexon/Nexon.php"><h2>Tata Nexon</h2></a>
+      <p>4.7/5 (390 Ratings)</p>
+      <p>17-24 kmpl | 113-118 bhp</p>
+      <p>Starting at Rs. 8.15 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="punch.jpg" alt="Tata Punch">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/tata/Punch/Punch.php"><h2>Tata Punch</h2></a>
+      <p>4.3/5 (353 Ratings)</p>
+      <p>18-26 kmpl | 72-87 bhp</p>
+      <p>Starting at Rs. 6.13 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="safari.jpg" alt="Tata Safari">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/tata/Safari/Safari.php"><h2>Tata Safari</h2></a>
+      <p>4.8/5 (390 Ratings)</p>
+      <p>14-16 kmpl | 168 bhp</p>
+      <p>Starting at Rs. 16.19 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="tiago.jpg" alt="Tata Tiago">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/tata/Tiago/Tiago.php"><h2>Tata Tiago</h2></a>
+      <p>4.4/5 (353 Ratings)</p>
+      <p>19-28 kmpl | 72-85 bhp</p>
+      <p>Starting at Rs. 5.65 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="tigor.jpg" alt="Tata Tigor">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/tata/Tigor/Tigor.php"><h2>Tata Tigor</h2></a>
+      <p>4.5/5 (390 Ratings)</p>
+      <p>19-28 kmpl | 72-85 bhp</p>
+      <p>Starting at Rs. 6.30 Lakh</p>
+    </div>
+  </div>
+
+  <!--mahindra cars-->
+  <div class="car-container">
+    <div class="car-image">
+      <img src="scorpio.jpg" alt="Mahindra Scorpio">
+    </div>
+    <div class="car-info">
+      <a href="/AutoHub/index/mahindra/Scorpio/Scorpio.php"><h2>Mahindra Scorpio</h2></a>
+      <p>4.8/5 (390 Ratings)</p>
+      <p>15 kmpl | 130 bhp</p>
+      <p>Starting at Rs. 13.59 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="bolero.jpg" alt="Mahindra Bolero">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/mahindra/bolero/Bolero.php"><h2>Mahindra Bolero</h2></a>
+      <p>4.6/5 (353 Ratings)</p>
+      <p>16 kmpl | 98 bhp</p>
+      <p>Starting at Rs. 11 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="tuv300.jpg" alt="Mahindra TUV300">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/mahindra/tuv300/tuv300.php"><h2>Mahindra TUV300</h2></a>
+      <p>4.5/5 (390 Ratings)</p>
+      <p>18 kmpl | 76-99 bhp</p>
+      <p>Starting at Rs. 8.10 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="xuv300.jpg" alt="Mahindra XUV300">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/mahindra/xuv300/xuv300.php"><h2>Mahindra XUV300</h2></a>
+      <p>4.4/5 (353 Ratings)</p>
+      <p>17-20 kmpl | 105-119 bhp</p>
+      <p>Starting at Rs. 7.99 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="xuv500.jpg" alt="Mahindra XUV500">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/mahindra/xuv500/xuv500.php"><h2>Mahindra XUV500</h2></a>
+      <p>4.5/5 (390 Ratings)</p>
+      <p>15 kmpl | 177 bhp</p>
+      <p>Starting at Rs. 12 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="xuv700.jpg" alt="Mahindra XUV700">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/mahindra/xuv700/xuv700.php"><h2>Mahindra XUV700</h2></a>
+      <p>4.7/5 (353 Ratings)</p>
+      <p>10-15 kmpl | 153-197 bhp</p>
+      <p>Starting at Rs. 13.99 Lakh</p>
+    </div>
+  </div>
+
+  <!--Hyundai cars-->
+<div class="car-container">
+    <div class="car-image">
+      <img src="creta.jpg" alt="Hyundai Creta">
+    </div>
+    <div class="car-info">
+      <a href="/AutoHub/index/hyundai/creta/Creta.php"><h2>Hyundai Creta</h2></a>
+      <p>4.6/5 (90 Ratings)</p>
+      <p>15-20 kmpl | 113-158 bhp</p>
+      <p>Starting at Rs. 11.00 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="venue.jpg" alt="Hyundai Venue">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/hyundai/venue/Venue.php"><h2>Hyundai Venue</h2></a>
+      <p>4.6/5 (253 Ratings)</p>
+      <p>17-23 kmpl | 82-118 bhp</p>
+      <p>Starting at Rs. 7.94 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="verna.jpg" alt="Hyundai Verna">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/hyundai/verna/Verna.php"><h2>Hyundai Verna</h2></a>
+      <p>4.5/5 (390 Ratings)</p>
+      <p>18-20 kmpl | 113-158 bhp</p>
+      <p>Starting at Rs. 11.00 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="alcazar.jpg" alt="Alcazar">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/hyundai/alcazar/Alcazar.php"><h2>Hyundai Alcazar</h2></a>
+      <p>4.4/5 (53 Ratings)</p>
+      <p>14-20 kmpl | 113-158 bhp</p>
+      <p>Starting at Rs. 16.77 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="i10.jpg" alt="i10">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/hyundai/i10/i10.php"><h2>Hyundai i10</h2></a>
+      <p>4.6/5 (390 Ratings)</p>
+      <p>17-24 kmpl | 68-82 bhp</p>
+      <p>Starting at Rs. 5.92 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="i20.jpg" alt="Mahindra i20">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/hyundai/i20/i20.php"><h2>Hyundai i20</h2></a>
+      <p>4.7/5 (353 Ratings)</p>
+      <p>20-24 kmpl | 82-87 bhp</p>
+      <p>Starting at Rs. 7.04 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="elantra.jpg" alt="elantra">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/hyundai/elantra/Elantra.php"><h2>Hyundai Elantra (Discontinued)</h2></a>
+      <p>4.6/5 (390 Ratings)</p>
+      <p>17-24 kmpl | 113-120 bhp</p>
+      <p>Starting at Rs. 15.92 Lakh</p>
+    </div>
+  </div>
+
+  <!--toyota cars-->
+  <div class="car-container">
+    <div class="car-image">
+      <img src="camry.jpg" alt="Toyota Camry">
+    </div>
+    <div class="car-info">
+      <a href="/AutoHub/index/toyota/camry/Camry.php"><h2>Toyota Camry</h2></a>
+      <p>4.6/5 (39 Ratings)</p>
+      <p>19 kmpl | 176 bhp</p>
+      <p>Starting at Rs. 46.17 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="glanza.jpg" alt="Toyota Glanza">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/toyota/glanza/Glanza.php"><h2>Toyota Glanza</h2></a>
+      <p>4.6/5 (35 Ratings)</p>
+      <p>22-30 kmpl | 76-89 bhp</p>
+      <p>Starting at Rs. 6.86 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="crysta.jpg" alt="Toyota Crysta">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/toyota/crysta/innova_crysta.php"><h2>Toyota Innova Crysta</h2></a>
+      <p>4.8/5 (139 Ratings)</p>
+      <p>17-24 kmpl | 148 bhp</p>
+      <p>Starting at Rs. 19.99 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="hycross.jpg" alt="Toyota Hycross">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/toyota/hycross/Innova_hycross.php"><h2>Toyota Innova Hycross</h2></a>
+      <p>4.5/5 (53 Ratings)</p>
+      <p>16-23 kmpl | 172-184 bhp</p>
+      <p>Starting at Rs. 19.77 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="landcruiser.jpg" alt="Toyota Land Cruiser">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/toyota/landcruiser/Land_cruiser.php"><h2>Toyota Land Cruiser</h2></a>
+      <p>4.8/5 (51 Ratings)</p>
+      <p>7-10 kmpl | 304 bhp</p>
+      <p>Starting at Rs. 2.10 cr</p>
+    </div>
+  </div>
+
+  <!--Honda cars-->
+  <div class="car-container">
+    <div class="car-image">
+      <img src="city.jpg" alt="City">
+    </div>
+    <div class="car-info">
+      <a href="/AutoHub/index/honda/city/city.php"><h2>Honda City</h2></a>
+      <p>4.4/5 (105 Ratings)</p>
+      <p>17-18 kmpl | 119 bhp</p>
+      <p>Starting at Rs. 11.74 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="civic.jpg" alt="civic">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/honda/civic/civic.php"><h2>Honda Civic</h2></a>
+      <p>4.6/5 (353 Ratings)</p>
+      <p>16-25 kmpl | 100 bhp</p>
+      <p>Starting at Rs. 18.00 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="amaze.jpg" alt="amaze">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/honda/amaze/amaze.php"><h2>Honda Amaze</h2></a>
+      <p>4.3/5 (90 Ratings)</p>
+      <p>18-24 kmpl | 79-98 bhp</p>
+      <p>Starting at Rs. 7.18 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="elevate.jpg" alt="Elevate">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/honda/elevate/elevate.php"><h2>Honda Elevate</h2></a>
+      <p>4.4/5 (353 Ratings)</p>
+      <p>15-16 kmpl | 119 bhp</p>
+      <p>Starting at Rs. 11.56 Lakh</p>
+    </div>
+  </div>
+
+  <!--Premium cars-->
+  <div class="car-container">
+    <div class="car-image">
+      <img src="aston.jpg" alt="aston martin">
+    </div>
+    <div class="car-info">
+      <a href="/AutoHub/index/exotics/astonmartin/astonmartin.php"><h2>Aston Martin DBS770</h2></a>
+      <p>4.6/5 </p>
+      <p>754 bhp</p>
+      <p>Starting at Rs. 1.30 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="rolls.jpg" alt="rolls royce">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/rolls/rolls.php"><h2>Rolls Royce Phantom</h2></a>
+      <p>4.6/5 </p>
+      <p>563 bhp</p>
+      <p>Starting at Rs. 8.00 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="bugatti.jpg" alt="bugatti chiron">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/bugatti/buggati.php"><h2>Bugatti Chiron</h2></a>
+      <p>4.5/5</p>
+      <p>1479 bhp</p>
+      <p>Starting at Rs. 19.00 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="porsche.jpg" alt="porsche">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/porsche/porsche.php"><h2>Porsche 911 GT3R</h2></a>
+      <p>4.4/5 </p>
+      <p>502 bhp</p>
+      <p>Starting at Rs. 2.75 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="ferrari.jpg" alt="ferrari">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/ferrari/ferrari.php"><h2>Ferrari Roma</h2></a>
+      <p>4.6/5 </p>
+      <p>611 bhp</p>
+      <p>Starting at Rs. 3.76 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="pagani.jpg" alt="pagani">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/pagani/pagani.php"><h2>Pagani Huayra</h2></a>
+      <p>4.7/5</p>
+      <p>754 bhp</p>
+      <p>Starting at Rs. 15.00 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="agera.jpg" alt="agera">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/agera/agera.php"><h2>Koenigsegg Agera</h2></a>
+      <p>4.6/5</p>
+      <p>1160 bhp</p>
+      <p>Starting at Rs. 12.00 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="audi.jpg" alt="audi">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/audi/audi.php"><h2>Audi R8</h2></a>
+      <p>4.7/5 </p>
+      <p>602 bhp</p>
+      <p>Starting at Rs. 2.72 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="bentley.jpg" alt="bentley">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/bentley/bentley.php"><h2>Bentley Continental GT</h2></a>
+      <p>4.7/5</p>
+      <p>626 bhp</p>
+      <p>Starting at Rs. 4.00 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="bmw.jpg" alt="bmw">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/bmw/bmw.php"><h2>BMW i8</h2></a>
+      <p>3.7/5 </p>
+      <p>260 bhp</p>
+      <p>Starting at Rs. 2.14 Cr </p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="dodge.jpg" alt="dodge">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/dodge/dodge.php"><h2>Dodge Challenger</h2></a>
+      <p>4.7/5</p>
+      <p>717 bhp</p>
+      <p>Starting at Rs. 1.30 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="g63.jpg" alt="g63">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/gwagon/gwagon.php"><h2>Mercedes G63</h2></a>
+      <p>4.7/5</p>
+      <p>600 bhp</p>
+      <p>Starting at Rs. 3.00 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="maybach.jpg" alt="maybach">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/maybach/maybach.php"><h2>Mercedes Maybach</h2></a>
+      <p>4.4/5 </p>
+      <p>600 bhp</p>
+      <p>Starting at Rs. 3.00 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="mustang.jpg" alt="mustang">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/mustang/mustang.php"><h2>Mustang GT500</h2></a>
+      <p>4.7/5</p>
+      <p>717 bhp</p>
+      <p>Starting at Rs. 1.30 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="urus.jpg" alt="urus">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/urus/urus.php"><h2>Lamborghini Urus </h2></a>
+      <p>4.8/5</p>
+      <p>600 bhp</p>
+      <p>Starting at Rs. 4.22 Cr</p>
+    </div>
+  </div>
+
+</body>
+</html>
