e8851a4e0e2f9772bb70562635f8cf9248be78fd
VishalWorksX <vishalkori1209@gmail.com>
2026-03-22 22:24:52 +0530

    Create honda.php

---
+++ Car Listing/honda/honda.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Honda Cars</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="honda.css">
+</head>
+<body>
+  <div class="car-container">
+    <div class="car-image">
+      <img src="city.jpg" alt="City">
+    </div>
+    <div class="car-info">
+      <a href="/AutoHub/index/honda/city/city.php"><h2>Honda City</h2></a>
+      <p>4.4/5 (105 Ratings)</p>
+      <p>17-18 kmpl | 119 bhp</p>
+      <p>Starting at Rs. 11.74 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="civic.jpg" alt="civic">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/honda/civic/civic.php"><h2>Honda Civic</h2></a>
+      <p>4.6/5 (353 Ratings)</p>
+      <p>16-25 kmpl | 100 bhp</p>
+      <p>Starting at Rs. 18.00 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="amaze.jpg" alt="amaze">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/honda/amaze/amaze.php"><h2>Honda Amaze</h2></a>
+      <p>4.3/5 (90 Ratings)</p>
+      <p>18-24 kmpl | 79-98 bhp</p>
+      <p>Starting at Rs. 7.18 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="elevate.jpg" alt="Elevate">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/honda/elevate/elevate.php"><h2>Honda Elevate</h2></a>
+      <p>4.4/5 (353 Ratings)</p>
+      <p>15-16 kmpl | 119 bhp</p>
+      <p>Starting at Rs. 11.56 Lakh</p>
+    </div>
+  </div>
+
+</body>
+</html>
