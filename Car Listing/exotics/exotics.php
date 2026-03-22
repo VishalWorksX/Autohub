4f921243b5e0bf7936d072c1934d51257e108112
VishalWorksX <vishalkori1209@gmail.com>
2026-03-22 22:23:41 +0530

    Create exotics.php

---
+++ Car Listing/exotics/exotics.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Premium Cars</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="exotics.css">
+</head>
+<body>
+  <div class="car-container">
+    <div class="car-image">
+      <img src="aston.jpg" alt="aston martin">
+    </div>
+    <div class="car-info">
+      <a href="/AutoHub/index/exotics/astonmartin/astonmartin.php"><h2>Aston Martin DBS770</h2></a>
+      <p>4.6/5 </p>
+      <p>754 bhp</p>
+      <p>Starting at Rs. 1.30 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="rolls.jpg" alt="rolls royce">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/rolls/rolls.php"><h2>Rolls Royce Phantom</h2></a>
+      <p>4.6/5 </p>
+      <p>563 bhp</p>
+      <p>Starting at Rs. 8.00 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="bugatti.jpg" alt="bugatti chiron">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/bugatti/buggati.php"><h2>Bugatti Chiron</h2></a>
+      <p>4.5/5</p>
+      <p>1479 bhp</p>
+      <p>Starting at Rs. 19.00 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="porsche.jpg" alt="porsche">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/porsche/porsche.php"><h2>Porsche 911 GT3R</h2></a>
+      <p>4.4/5 </p>
+      <p>502 bhp</p>
+      <p>Starting at Rs. 2.75 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="ferrari.jpg" alt="ferrari">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/ferrari/ferrari.php"><h2>Ferrari Roma</h2></a>
+      <p>4.6/5 </p>
+      <p>611 bhp</p>
+      <p>Starting at Rs. 3.76 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="pagani.jpg" alt="pagani">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/pagani/pagani.php"><h2>Pagani Huayra</h2></a>
+      <p>4.7/5</p>
+      <p>754 bhp</p>
+      <p>Starting at Rs. 15.00 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="agera.jpg" alt="agera">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/agera/agera.php"><h2>Koenigsegg Agera</h2></a>
+      <p>4.6/5</p>
+      <p>1160 bhp</p>
+      <p>Starting at Rs. 12.00 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="audi.jpg" alt="audi">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/audi/audi.php"><h2>Audi R8</h2></a>
+      <p>4.7/5 </p>
+      <p>602 bhp</p>
+      <p>Starting at Rs. 2.72 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="bentley.jpg" alt="bentley">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/bentley/bentley.php"><h2>Bentley Continental GT</h2></a>
+      <p>4.7/5</p>
+      <p>626 bhp</p>
+      <p>Starting at Rs. 4.00 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="bmw.jpg" alt="bmw">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/bmw/bmw.php"><h2>BMW i8</h2></a>
+      <p>3.7/5 </p>
+      <p>260 bhp</p>
+      <p>Starting at Rs. 2.14 Cr </p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="dodge.jpg" alt="dodge">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/dodge/dodge.php"><h2>Dodge Challenger</h2></a>
+      <p>4.7/5</p>
+      <p>717 bhp</p>
+      <p>Starting at Rs. 1.30 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="g63.jpg" alt="g63">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/gwagon/gwagon.php"><h2>Mercedes G63</h2></a>
+      <p>4.7/5</p>
+      <p>600 bhp</p>
+      <p>Starting at Rs. 3.00 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="maybach.jpg" alt="maybach">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/maybach/maybach.php"><h2>Mercedes Maybach</h2></a>
+      <p>4.4/5 </p>
+      <p>600 bhp</p>
+      <p>Starting at Rs. 3.00 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="mustang.jpg" alt="mustang">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/mustang/mustang.php"><h2>Mustang GT500</h2></a>
+      <p>4.7/5</p>
+      <p>717 bhp</p>
+      <p>Starting at Rs. 1.30 Cr</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="urus.jpg" alt="urus">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/exotics/urus/urus.php"><h2>Lamborghini Urus </h2></a>
+      <p>4.8/5</p>
+      <p>600 bhp</p>
+      <p>Starting at Rs. 4.22 Cr</p>
+    </div>
+  </div>
+
+  
+</body>
+</html>
