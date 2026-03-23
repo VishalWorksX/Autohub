adcda3bc2edbab3c1ba627e2f79dbaa4e39927d8
VishalWorksX <vishalkori1209@gmail.com>
2026-03-23 13:41:57 +0530

    Add files via upload

---
+++ Car Listing/hyundai.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Hyundai Cars</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="hyundai.css">
+</head>
+<body>
+  <div class="car-container">
+    <div class="car-image">
+      <img src="creta.jpg" alt="Hyundai Creta">
+    </div>
+    <div class="car-info">
+      <a href="/AutoHub/index/hyundai/creta/Creta.php"><h2>Hyundai Creta</h2></a>
+      <p>4.6/5 (90 Ratings)</p>
+      <p>15-20 kmpl | 113-158 bhp</p>
+      <p>Starting at Rs. 11.00 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="venue.jpg" alt="Hyundai Venue">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/hyundai/venue/Venue.php"><h2>Hyundai Venue</h2></a>
+      <p>4.6/5 (253 Ratings)</p>
+      <p>17-23 kmpl | 82-118 bhp</p>
+      <p>Starting at Rs. 7.94 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="verna.jpg" alt="Hyundai Verna">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/hyundai/verna/Verna.php"><h2>Hyundai Verna</h2></a>
+      <p>4.5/5 (390 Ratings)</p>
+      <p>18-20 kmpl | 113-158 bhp</p>
+      <p>Starting at Rs. 11.00 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="alcazar.jpg" alt="Alcazar">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/hyundai/alcazar/Alcazar.php"><h2>Hyundai Alcazar</h2></a>
+      <p>4.4/5 (53 Ratings)</p>
+      <p>14-20 kmpl | 113-158 bhp</p>
+      <p>Starting at Rs. 16.77 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="i10.jpg" alt="i10">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/hyundai/i10/i10.php"><h2>Hyundai i10</h2></a>
+      <p>4.6/5 (390 Ratings)</p>
+      <p>17-24 kmpl | 68-82 bhp</p>
+      <p>Starting at Rs. 5.92 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="i20.jpg" alt="Mahindra i20">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/hyundai/i20/i20.php"><h2>Hyundai i20</h2></a>
+      <p>4.7/5 (353 Ratings)</p>
+      <p>20-24 kmpl | 82-87 bhp</p>
+      <p>Starting at Rs. 7.04 Lakh</p>
+    </div>
+  </div>
+
+  <div class="car-container">
+    <div class="car-image">
+      <img src="elantra.jpg" alt="elantra">
+    </div>
+    <div class="car-info">
+    <a href="/AutoHub/index/hyundai/elantra/Elantra.php"><h2>Hyundai Elantra (Discontinued)</h2></a>
+      <p>4.6/5 (390 Ratings)</p>
+      <p>17-24 kmpl | 113-120 bhp</p>
+      <p>Starting at Rs. 15.92 Lakh</p>
+    </div>
+  </div>
+</body>
+</html>
